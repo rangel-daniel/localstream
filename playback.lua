4dcf4ce89a313dad3c1c8d37921b95df27b621a7
Daniel Rangel <000.drf@gmail.com>
2023-12-15 11:02:02 -0800

    Playback module started!

---
+++ playback.lua
@@ -0,0 +1,64 @@
+local mp = require("mp")
+local playback = {
+	loaded = false,
+}
+
+local function findIndex(isPreloaded, session, filename)
+	if not filename then
+		return 1
+	end
+
+	local epList = mp.get_property_native("playlist", {})
+	local ep = nil
+
+	if isPreloaded then
+		ep = "./" .. filename
+	else
+		ep = session .. "/" .. filename
+	end
+
+	local lo, hi = 1, #epList
+
+	while lo <= hi do
+		local mid = math.floor((lo + hi) / 2)
+		local curr = epList[mid]
+
+		if curr.filename == ep then
+			return mid
+		elseif curr.filename < ep then
+			lo = mid + 1
+		else
+			hi = mid - 1
+		end
+	end
+	return 1
+end
+
+function playback:new(params)
+	if not params.session then
+		return
+	end
+	local timeout = 0.25
+	if not params.isPreloaded then
+		mp.commandv("loadlist", params.session, "replace")
+		timeout = 0.5
+	end
+
+	mp.add_timeout(timeout, function()
+        local index = findIndex(params.isPreloaded, params.session, params.filename)
+		if index > 1 then
+			mp.set_property("playlist-pos", index - 1)
+		end
+
+		if params["time-pos"] then
+			mp.add_timeout(0.1, function()
+				mp.commandv("seek", params["time-pos"], "absolute", "exact")
+				playback.loaded = true
+			end)
+		else
+			playback.loaded = true
+		end
+    end)
+end
+
+return playback
